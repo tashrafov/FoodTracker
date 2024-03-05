5d8176a2291b1e050e14345bda0eab58c28be73b
Taghi Ashrafov <tagi.ashrafov@kapitalbank.az>
2024-03-05 12:17:42 +0400

    added feature modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "FoodTracker"
 include(":app")
+include(":core")
+include(":onboarding")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":tracker")
+include(":tracker:tracker_data")
+include(":tracker:tracker_domain")
+include(":tracker:tracker_presentation")
